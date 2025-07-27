3ae6e6b726cc395661d3e5c8a1d45d074abff05b
baydakov-georgiy <lespend33@gmail.com>
2025-07-27 20:02:25 +0500

    added metasymbol ?

--- main.c
+++ main.c
@@ -13,13 +13,25 @@ bool matchstar(int c, const char *regexp, const char *text) {
     return false;
 };
 
+bool matchplus(int c, const char *regexp, const char *text) {
+    return regexp[0] == *text++ && matchstar(regexp[0], regexp + 2, text);
+}
+
+bool matchquestion(int c, const char *regexp, const char *text) {
+    if (*text != '\0' && (*text == c || c == '.'))
+        return matchhere(regexp, text + 1);
+    return matchhere(regexp, text);
+}
+
 bool matchhere(const char *regexp, const char *text) {
     if (regexp[0] == '\0')
         return true;
     if (regexp[1] == '*')
         return matchstar(regexp[0], regexp + 2, text);
     if (regexp[1] == '+')
-        return regexp[0] == *text++ && matchstar(regexp[0], regexp + 2, text);
+        return matchplus(regexp[0], regexp, text);
+    if (regexp[1] == '?')
+        return matchquestion(regexp[0], regexp + 2, text);
     if (regexp[0] == '$' && regexp[1] == '\0')
         return *text == '\0';
     if (*text != '\0' && (regexp[0] == '.' || regexp[0] == *text))
@@ -38,6 +50,7 @@ bool match(const char *regexp, const char *text) {
 };
 
 int main() {
+    assert(match("aba", "ababa") == true);
     assert(match("^aba$", "ababa") == false);
     assert(match("a*.c", "abab124124lla") == false);
     assert(match("a*a", "abab124124lla") == true);
@@ -47,5 +60,13 @@ int main() {
     assert(match("a+", "aa") == true);
     assert(match("ba+", "baaa") == true);
     assert(match("ba+q", "baaaq") == true);
+    assert(match("ba+q", "baaa") == false);
     assert(match("ba+q", "bq") == false);
+    assert(match("ba?q", "baq") == true);
+    assert(match("ba?q", "bq") == true);
+    assert(match("ba3?q", "ba3q") == true);
+    assert(match("b?b?", "b") == true);
+    assert(match("^b?b?$", "aa") == false);
+
+
 }
